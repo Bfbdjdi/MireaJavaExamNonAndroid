7df3d752a2bb51cc396a3de6e032ea5252fe68f1
Salty <goognis235@gmail.com>
2024-06-20 13:58:54 +0300

    Сделано первое задание

---
+++ src/main/java/oaosalty/FirstLineConverter.java
@@ -0,0 +1,40 @@
+package oaosalty;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class FirstLineConverter {
+    public static void lineConverter()
+    {
+        System.out.println("Введи строку:");
+
+        //Считываем ввод в консоль
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        //Все 3 преобразования input в одной строке с помощью StringBuilder - тип как String, но изменяемый
+        String modifiedInput = new StringBuffer(input).reverse().replace(0, 1, "")
+                .replace(input.length()-2,input.length()-1,"").toString().toUpperCase();
+
+        try {
+            //Создаётся папка на диске. Возможно, придётся запустить IDE от имени администратора
+            Path path = Paths.get("C:\\FirstLineConverter\\");
+            Files.createDirectories(path);
+
+            //Создаём поток для записи в файл
+            FileWriter fw = new FileWriter("C:\\FirstLineConverter\\output.txt");
+
+            //Пишем в файл
+            fw.write(modifiedInput);
+
+            //Закрываем поток
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("\nНе удалось записать строку в файл.\n");
+        }
+    }
+}
---
+++ src/main/java/oaosalty/Main.java
@@ -0,0 +1,54 @@
+package oaosalty;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true)
+        {
+            System.out.println("\nВведи номер кода для запуска:\n" +
+                    "1. Изменение строки\n" +
+                    "2. МаксМинЗнач\n" +
+                    "3. Сортировка Person\n" +
+                    "4. Сортировка HashMap\n" +
+                    "5. Удалить гласные из файла\n" +
+                    "6. Сериализация\n" +
+                    "7. Площадь прямоугольника\n" +
+                    "8. Факториал\n" +
+                    "9. Ошибка в коде\n" +
+                    "10. Сайт\n");
+
+            Scanner scan = new Scanner(System.in);
+            String input = scan.nextLine();
+
+            switch (input)
+            {
+                case "1":
+                    FirstLineConverter.lineConverter();
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                case "8":
+                    break;
+                case "9":
+                    break;
+                case "10":
+                    break;
+
+                default:
+                    System.out.println("Нужно ввести только число.");
+                    break;
+            }
+        }
+    }
+}
